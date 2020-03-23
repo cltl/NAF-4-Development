27f1236b8e3611d80bc423e68caadc085a86aa7c
MartenPostma <martenp@gmail.com>
2020-03-23 12:03:30 +0100

    worked on coreference

--- scripts/coreference.sh
+++ scripts/coreference.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # coreference of two mentions referring to Incident URI
-python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/before/coreference/coreference.naf" --naf_v4_dtd_path="../resources/naf development/naf_v4.dtd"
-python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/after/coreference/coreference.naf" --naf_v4_dtd_path="../resources/naf development/naf_v4.dtd"
+python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/before/coreference/coreference.naf" --naf_v4_dtd_path="../resources/naf_development/naf_v4.dtd"
+python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/after/coreference/coreference.naf" --naf_v4_dtd_path="../resources/naf_development/naf_v4.dtd"
+python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/after/coreference/update_coreference.naf" --naf_v4_dtd_path="../resources/naf_development/naf_v4.dtd"
+python validate_against_dtd.py --naf_path="../resources/data-to-text annotation tool/NAF/after/coreference/deprecate_coreference.naf" --naf_v4_dtd_path="../resources/naf_development/naf_v4.dtd"
